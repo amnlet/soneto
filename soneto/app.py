b2b297aa7444e6b8d63a948c6750024fd1b1a53f
amnlet <misteriodrey@gmail.com>
2023-02-21 17:02:45 -0300

    mudança de diretorio para soneto\soneto.txt

--- soneto/app.py
+++ soneto/app.py
@@ -1,10 +1,10 @@
-with open("soneto.txt", "r") as f:
+with open("soneto\soneto.txt", "r") as f:
     linhas = f.read().splitlines() #Aqui ele vai ler e separar as linhas e tira o \n
     matriz = {} #aqui estou usando o dictionary eu tentei [] por muito tempo o chat GPT me indicou isso, mas precisava entender
     for linha in linhas:
         palavra_de_cada_linha = linha.split() #aqui ele verifica todas as linhas e separa elas
         for palavra in palavra_de_cada_linha:
-            palavra = palavra.strip(",.").lower() #aqui ele separa todas as plavaras de cada linha
+            palavra = palavra.strip(",.").lower() #aqui ele separa todas as plavaras de cada linha e deixa caps lower
             if palavra in matriz:
                 matriz[palavra] += 1 #aqui ele soma para cada palavra dentro da dictionary(matriz) uma pontuacao por palavras repetidas
             else:
