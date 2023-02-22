8316e6d2394890fea9168d3ad57dded9197b14f2
rodrigmars <rodrigmars@gmail.com>
2023-02-22 08:12:13 -0300

    Módulo app2 refatorado

--- soneto/app2.py
+++ soneto/app2.py
@@ -1,29 +1,24 @@
 from re import sub
+from typing import Callable, List
+from collections import OrderedDict
 
-def processa_soneto(file:str):
-    
-    def gera_lista_palavras():
+
+def processa_soneto(file: str) -> tuple[Callable[[List[str]], List[str]], Callable[[], List[str]]]:
+
+    def gera_lista_palavras() -> List[str]:
         
         pattern = "[^A-Za-z\\s]"
 
         with open(file, "r") as f:
             return sub(pattern, "", f.read().lower()).split()
-    
-    def total_ocorrencias(lista_palavras:list[str]):
 
-            lista_palavras = gera_lista_palavras()
+    def total_ocorrencias(lista_palavras:List[str]) -> List[str]:
 
-            num_palavras = list(map(lambda x:[x, lista_palavras.count(x)], lista_palavras))
+        palavras = list(OrderedDict.fromkeys(map(lambda x:f"{x}:{lista_palavras.count(x)}", lista_palavras)))
 
-            palavras = []
+        palavras.sort(key=lambda w:w.split(":")[1], reverse=True)
 
-            [palavras.append(item) for item in num_palavras if item not in palavras]
-            
-            palavras.sort(key=lambda x:x[1], reverse=True)
-
-            for palavra in palavras:
-                print(f"{palavra[0]:10}:{palavra[1]}")
-                
+        return palavras
 
     return total_ocorrencias, gera_lista_palavras
 
@@ -31,4 +26,5 @@ if __name__ == "__main__":
 
     total_ocorrencias, palavras = processa_soneto("soneto/soneto.txt")
     
-    total_ocorrencias(palavras())
+    for palavra in total_ocorrencias(palavras()):
+        print(palavra)
