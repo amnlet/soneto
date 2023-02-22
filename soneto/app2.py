ebbad30593ef75aa29bd1a8ea340d89b1e65bcfb
rodrigmars <rodrigmars@gmail.com>
2023-02-22 11:40:50 -0300

    Tratamento de execeção incluído

--- soneto/app2.py
+++ soneto/app2.py
@@ -1,9 +1,17 @@
+from sys import exit
 from re import sub
-from typing import Callable, List
+from traceback import print_exc
+
+from typing import Callable, List, Iterable
 from collections import OrderedDict
 
+Soneto = tuple[Callable[[list[str]], list[str]],
+               Callable[[Iterable[str]], list[str]],
+               Callable[[list[str]], Iterable[str]],
+               Callable[[], list[str]]]
 
-def processa_soneto(file: str) -> tuple:
+
+def processa_soneto(file: str) -> Soneto:
 
     def gera_lista_palavras() -> List[str]:
 
@@ -12,10 +20,10 @@ def processa_soneto(file: str) -> tuple:
         with open(file, "r") as f:
             return sub(pattern, "", f.read().lower()).split()
 
-    def remove_duplicidade(palavras: map):
+    def remove_duplicidade(palavras: Iterable[str]) -> List[str]:
         return list(OrderedDict.fromkeys(palavras))
 
-    def conta_palavras(palavras: List[str]):
+    def conta_palavras(palavras: List[str]) -> Iterable[str]:
         return map(lambda p: f"{p}:{palavras.count(p)}", palavras)
 
     def total_ocorrencias(palavras: List[str]) -> List[str]:
@@ -32,17 +40,26 @@ def processa_soneto(file: str) -> tuple:
 
 if __name__ == "__main__":
 
-    total_ocorrencias, \
-        remove_duplicidade, \
-        conta_palavras, \
-        gera_lista_palavras = processa_soneto(
-            "soneto/soneto_de_fidelidade.txt")
+    try:
 
-    palavras = total_ocorrencias(remove_duplicidade(
-        conta_palavras(gera_lista_palavras())))
+        total_ocorrencias, \
+            remove_duplicidade, \
+            conta_palavras, \
+            gera_lista_palavras = processa_soneto(
+                "soneto/soneto_de_fidelidade.txt")
 
-    for palavra in palavras:
-        palavra_, total = palavra.split(':')
+        palavras = total_ocorrencias(remove_duplicidade(
+            conta_palavras(gera_lista_palavras())))
 
-        if int(total) > 1:
-            print(f"Palavra: {palavra_:13} = {total}")
+        for palavra in palavras:
+            palavra_, total = palavra.split(':')
+
+            if int(total) > 1:
+                print(f"Palavra: {palavra_:13} = {total}")
+
+        exit(0)
+
+    except Exception:
+        print_exc()
+
+        exit(1)
